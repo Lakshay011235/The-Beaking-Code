778d76bf29889340da3351c38c00742144150a15
Anirudh <anirudhmukkamala@gamil.com>
2022-12-22 13:38:25 +0530

    Initialized

---
+++ env_in/Green_4_U/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Green4UConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Green_4_U'
---
+++ env_in/Other_platforms/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OtherPlatformsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Other_platforms'
